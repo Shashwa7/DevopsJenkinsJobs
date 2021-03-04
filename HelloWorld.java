f68e5907e36b9e1b69ef3ae6fafc95e3c016a275
Shashwa7 <rextrip4@gmail.com>
2021-03-04 15:48:49 +0530

    First JenkingJob with git integration. @shashwa7

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld 
+{
+       public static void main (String[] args)
+       {
+             // Ausgabe Hello World!
+             System.out.println("Hello World! Program  build and execution done by Jenkins with Git Integration!");
+       }
+}
